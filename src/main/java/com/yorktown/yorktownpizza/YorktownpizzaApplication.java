f7ec000c79a6f17d16e090481aaae55feb83fea6
puneetkumar <puneetrocks20@gmail.com>
2025-06-16 20:55:23 +0530

    initial commit

---
+++ src/main/java/com/yorktown/yorktownpizza/YorktownpizzaApplication.java
@@ -0,0 +1,13 @@
+package com.yorktown.yorktownpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorktownpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YorktownpizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yorktown/yorktownpizza/YorktownpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorktown.yorktownpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YorktownpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
